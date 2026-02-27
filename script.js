22f7c3089963a0e31d8ad5f6cd5a098163253be9
231Uzbek <antopoghosyan@gmail.com>
2026-02-27 22:49:17 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+let count = 0;
+const cartDisplay = document.getElementById('cart-count');
+
+// Логика кнопок "В корзину"
+document.querySelectorAll('.buy-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        count++;
+        cartDisplay.innerText = count;
+        alert('Добавлено в корзину!');
+    });
+});
+
+// Логика формы заказа
 document.getElementById('orderForm').addEventListener('submit', (e) => {
     e.preventDefault();
-    alert('Спасибо за заказ в AndShirt! Мы свяжемся с вами в ближайшее время.');
-    e.target.reset(); // Очистить форму
+    const name = document.getElementById('name').value;
+    alert('Спасибо, ' + name + '! Ваш заказ в AndShirt принят.');
+    e.target.reset();
 });
